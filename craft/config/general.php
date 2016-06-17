3ad772d038eacab57128d033baa76613ab166e4c
David Rampersad <daveramps@gmail.com>
2016-06-17 13:51:11 -0700

    turned off devmode for live site

--- craft/config/general.php
+++ craft/config/general.php
@@ -7,8 +7,18 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
+
 return array(
-    'devMode' => true,
-    'useEmailAsUsername' => true,
-    'autoLoginAfterAccountActivation' => true
+    '*' => array(
+        'useEmailAsUsername' => true,
+        'autoLoginAfterAccountActivation' => true
+    ),
+
+    'test.partneur.com' => array(
+        'devMode' => true,
+    ),
+
+    'partneur.com' => array(
+        'devMode' => false,
+    )
 );
